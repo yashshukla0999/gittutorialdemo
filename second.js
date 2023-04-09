21b3728a77cff4fb6d90651f4fb8b0b7dbc593ed
yashshukla099 <yashshukla0999@gmail.com>
2023-04-09 20:33:24 +0530

    javascripit code for getlementbyclasses name

--- second.js
+++ second.js
@@ -1,1 +1,5 @@
-console.log('finally add commit to git');
+const item = document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor='green';
+for(var i = 0;i<item.length;i++){
+    item[i].style.fontWeight ='bold';
+}
