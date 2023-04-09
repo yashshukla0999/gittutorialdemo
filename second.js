185b5ec4d079f7b2a131003488ed26db131a661d
yashshukla099 <yashshukla0999@gmail.com>
2023-04-10 00:02:02 +0530

    creating nodes

--- second.js
+++ second.js
@@ -8,13 +8,38 @@
 // v[4].style.backgroundColor='red';
 
 
-const back=document.querySelector('.list-group-item:nth-child(2)');
-back.style.backgroundColor='red';
-const visible=document.querySelector('.list-group-item:nth-child(3)');
-visible.style.display ='none';
-const color = document.querySelectorAll('.list-group-item');
-color[1].style.color='yellow';
-const bg= document.querySelectorAll('li:nth-child(odd)');
-for(let i = 0;i<bg.length;i++){
-    bg[i].style.backgroundColor='green';
-}
+// const back=document.querySelector('.list-group-item:nth-child(2)');
+// back.style.backgroundColor='red';
+// const visible=document.querySelector('.list-group-item:nth-child(3)');
+// visible.style.display ='none';
+// const color = document.querySelectorAll('.list-group-item');
+// color[1].style.color='yellow';
+// const bg= document.querySelectorAll('li:nth-child(odd)');
+// for(let i = 0;i<bg.length;i++){
+//     bg[i].style.backgroundColor='green';
+// }
+const item = document.querySelector('#items');
+
+console.log(item.parentElement.parentElement.parentElement);
+//first element
+item.firstElementChild.textContent='hello';
+console.log(item.firstElementChild);
+//last element
+item.lastElementChild.textContent='world';
+console.log(item.lastElementChild);
+//next siblings
+console.log(item.nextElementSibling);
+//revious sibling
+console.log(item.previousElementSibling);
+//
+var newdiv = document.createElement('div');
+newdiv.className = 'hello'
+newdiv.setAttribute('title','hello div');
+var newdivtext = document.createTextNode('Hello World')
+newdiv.appendChild(newdivtext);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newdiv,h1);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore('header h1');
